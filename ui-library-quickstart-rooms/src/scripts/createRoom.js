bc9a40a67ff5accc6638d07a0462c77fa4ab83b9
treytro <35743905+treytro@users.noreply.github.com>
2025-04-15 14:36:00 -0500

    Delete ui-library-quickstart-rooms directory

--- ui-library-quickstart-rooms/src/scripts/createRoom.js
+++
@@ -1,4 +0,0 @@
-var { RoomsClient } = require("@azure/communication-rooms");
-var { CONNECTION_STRING } = require("./settings");
-var roomsClient = new RoomsClient(CONNECTION_STRING);
-roomsClient.createRoom().then(function (res) { return console.log(res); });
