1a1ba487d282590d5a55bf0012aaf587bedfef65
treytro <35743905+treytro@users.noreply.github.com>
2025-04-15 14:25:12 -0500

    Delete ui-library-call-readiness directory

--- ui-library-call-readiness/src/helpers/environmentSupportUtils.ts
+++
@@ -1,6 +0,0 @@
-import { EnvironmentInfo, Features } from "@azure/communication-calling";
-import { StatefulCallClient } from "@azure/communication-react";
-
-/** Use the callClient's getEnvironmentInfo() method to check if the browser is supported. */
-export const checkEnvironmentSupport = async (callClient: StatefulCallClient): Promise<EnvironmentInfo> =>
-  await callClient.feature(Features.DebugInfo).getEnvironmentInfo();
