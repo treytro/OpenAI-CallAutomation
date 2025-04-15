e6802d14cd3eb74798e44b855adca56b6fca3bbd
treytro <35743905+treytro@users.noreply.github.com>
2025-04-15 14:24:59 -0500

    Delete ui-library-add-call-recording directory

--- ui-library-add-call-recording/app/src/Secrets.ts
+++
@@ -1,4 +0,0 @@
-// Generate a user and token with the "Voice and video calling (VOIP)" scopes.
-// and paste the secrets here.
-export const ACS_USER_ID = "REPLACE_WITH_USER_ID"
-export const ACS_TOKEN = "REPLACE_WITH_TOKEN"
